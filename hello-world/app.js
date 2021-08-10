895a67996a14b9cf025ff4139f48f1a715649f7a
DheerajGBhatt <dheeraj@antstack.io>
2021-08-10 06:08:01 +0530

    commit with small change

--- hello-world/app.js
+++ hello-world/app.js
@@ -14,13 +14,13 @@ let response;
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
-exports.lambdaHandler = async (event, context) => {
+exports.lambdaHandler = async(event, context) => {
     try {
         // const ret = await axios(url);
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'hello world test',
                 // location: ret.data.trim()
             })
         }
